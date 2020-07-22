7c1c9c61f7c8fae23446cd73cf288b8f3ab20f0a
Meir Gabay <meir@prodops.io>
2020-07-23 00:43:48 +0300

    print location in example

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source bargs.sh "$@"
 
-echo -e "Name:~$person_name\nAge:~$age\nGender:~$gender" | column -t -s "~"
+echo -e "Name:~$person_name\nAge:~$age\nGender:~$gender\nLocation:~$location" | column -t -s "~"
