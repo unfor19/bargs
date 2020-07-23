1a0bcaea302ab496e4180133eb9e77f5489aaea8
Meir Gabay <meir@prodops.io>
2020-07-23 13:16:23 +0300

    tests script

---
+++ tests.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+error_msg(){
+    local msg=$1
+    echo -e "[ERROR] $1"
+    exit
+}
+
+should(){
+    local expected=$1
+    local test_name=$2
+    local expr=$3
+    echo "-------------------------------------------------------"
+    echo "[LOG] $test_name - Should $expected"
+    output_msg=$(trap '$expr' EXIT)
+    output_code=$?
+
+    echo -e "[LOG] Output: \n$output_msg"
+
+    if [[ $expected == "pass" && $output_code -eq 0 ]]; then
+        echo "[LOG] Test passed as expected"
+    elif [[ $expected == "fail" && $output_code -eq 1 ]]; then
+        echo "[LOG] Test failed as expected"
+    else
+        error_msg "Test output is not expected, terminating"
+    fi
+}
+
+should pass "Default Values" "source example.sh -a 99 --gender male"
+should pass "New Values" "source example.sh -a 23 --gender male -l neverland -n meir"
+should fail "Empty Argument" "source example.sh -a 99 --gender"
+should fail "Unknown Argument"  "source example.sh -a 99 -u meir"
