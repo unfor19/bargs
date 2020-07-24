cc4e2acaef3f147afb52a797b3a72cdf7492ee9e
Meir Gabay <meir@prodops.io>
2020-07-24 12:14:30 +0300

    ignoring shellcheck error

--- example.sh
+++ example.sh
@@ -2,7 +2,7 @@
 source bargs.sh "$@"
 
 echo -e \
-"Name:~$PERSON_NAME\n"\
-"Age:~$AGE\n"\
-"Gender:~$GENDER\n"\
+"Name:~$PERSON_NAME\n"\ # shellcheck disable=SC1091
+"Age:~$AGE\n"\ # shellcheck disable=SC1091
+"Gender:~$GENDER\n"\ # shellcheck disable=SC1091
 "Location:~$LOCATION" | column -t -s "~"
