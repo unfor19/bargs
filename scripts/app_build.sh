eaa9099aab9ec1c0a620889a97307990aadd1913
Meir Gabay <meir@prodops.io>
2020-08-01 12:18:01 +0300

    build release

--- scripts/app_build.sh
+++ scripts/app_build.sh
@@ -10,8 +10,19 @@ find "${PROJECT_DIR}" -maxdepth 1 -type f \
     -iname "*.sh" -exec cp {} ${DIST_DIR} \;
 cp bargs_vars ${DIST_DIR}/
 
+# Any push
 [[ -z $GITHUB_SHA ]] && export GITHUB_SHA=$(git rev-parse HEAD)
-SHORT_COMMIT="${GITHUB_SHA:0:8}"
-echo "${SHORT_COMMIT}" > "${DIST_DIR}/version"
+SHORT_COMMIT="SHORT_COMMIT=${GITHUB_SHA:0:8}"
+COMMIT="COMMIT=${GITHUB_SHA}"
+echo -e "${COMMIT}\n${SHORT_COMMIT}" > "${DIST_DIR}/version"
+
+# Release
+if [[ -n "$GITHUB_REF" ]]; then
+    version=$(echo "${GITHUB_REF}" | sed "s|refs\/tags\/v||g")
+    [[ -n "$version" ]] && echo -e "VERSION=${version}" >> "${DIST_DIR}/version"
+    mkdir -p "${DIST_DIR}/${version}"
+    find "${DIST_DIR}" -maxdepth 1 -type f \
+        -exec cp {} "${DIST_DIR}/${version}" \;
+fi
 
 echo "[LOG] Finished building, artifacts in - ${DIST_DIR}"
