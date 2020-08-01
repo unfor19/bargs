1d5501c61077066bffe01a761e30586f7fe23612
Meir Gabay <meir@prodops.io>
2020-08-01 12:01:27 +0300

    version file

--- scripts/app_build.sh
+++ scripts/app_build.sh
@@ -3,7 +3,7 @@ PROJECT_DIR="${PWD}"
 DIST_DIR="${PROJECT_DIR}/dist"
 echo "[LOG] Building"
 [[ -d "${DIST_DIR}" ]] && rm -r "${DIST_DIR}"
-mkdir "${PROJECT_DIR}/dist"
+mkdir "${DIST_DIR}"
 
 echo "[LOG] Copying files to ${DIST_DIR}"
 find "${PROJECT_DIR}" -maxdepth 1 -type f \
@@ -12,16 +12,6 @@ cp bargs_vars ${DIST_DIR}/
 
 [[ -z $GITHUB_SHA ]] && export GITHUB_SHA=$(git rev-parse HEAD)
 SHORT_COMMIT="${GITHUB_SHA:0:8}"
-mkdir -p "${DIST_DIR}/${SHORT_COMMIT}"
-COMMIT_DIR="${PROJECT_DIR}/dist/${SHORT_COMMIT}"
-
-for f in ${DIST_DIR}/*; do
-    if [ -f "$f" ]; then
-        filename=$(basename ${f})
-        target_path="${COMMIT_DIR}/${filename}"
-        echo "[LOG] Copying $filename to ${target_path}"
-        cp "${f}" "${target_path}"
-    fi
-done
+echo "${SHORT_COMMIT}" > "${DIST_DIR}/version"
 
 echo "[LOG] Finished building, artifacts in - ${DIST_DIR}"
