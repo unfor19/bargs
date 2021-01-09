08325a39fef5f217169db44c84a46cf2df79d6cc
Meir Gabay <unfor19@gmail.com>
2021-01-09 02:31:29 +0200

    prettier example

--- example.sh
+++ example.sh
@@ -2,16 +2,16 @@
 source "${PWD}"/"$(dirname ${BASH_SOURCE[0]})"/bargs.sh "$@"
 
 echo "
-Name: ~ $person_name
-Age: ~ $age
-Gender: ~ $gender
-Location: ~ $location
-Favorite food: ~ $favorite_food
-Secret: ~ $secret
-Password: ~ $password
-OS Language: ~ $language
-I am happy: ~ $happy
-CI Process: ~ $CI
-Uppercased var names: ~ $PERSON_NAME, $AGE years old, from $LOCATION
+Name:                  ~ $person_name
+Age:                   ~ $age
+Gender:                ~ $gender
+Location:              ~ $location
+Favorite food:         ~ $favorite_food
+Secret:                ~ $secret
+Password:              ~ $password
+OS Language:           ~ $language
+I am happy:            ~ $happy
+CI Process:            ~ $CI
+Uppercased var names:  ~ $PERSON_NAME, $AGE years old, from $LOCATION
 Username from env var: ~ $username " \
     | column -t -s "~"
